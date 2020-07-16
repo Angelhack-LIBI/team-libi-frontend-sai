465a00dbf7c0b058704870eded4fe9f35d841c1d
Donghgo Gang <gongdongho12@gmail.com>
2020-07-17 00:45:57 +0900

    accessToken, refreshToken update.

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,33 @@
 import axios from 'axios'
+import createAuthRefreshInterceptor from 'axios-auth-refresh';
 
-const instance = axios.create({
+
+const axiosInstance = axios.create({
   baseURL: 'https://some-domain.com/api/',
-  timeout: 2000,
+  timeout: 2500,
   headers: {'Content-Type': 'application/json'}
 });
 
-export default instance
+const getAccessToken = () => localStorage.getItem('token');
+
+axiosInstance.interceptors.request.use(request => {
+  request.headers['Authorization'] = getAccessToken();
+  return request;
+});
+
+const refreshAuthLogic = (failedRequest: any) => {
+  return axiosInstance.post('https://www.example.com/auth/token/refresh')
+    .then(({ data = {} }) => {
+      const { token } = data || {}
+      localStorage.setItem('token', token);
+      failedRequest.response.config.headers['Authorization'] = token;
+      return Promise.resolve();
+    });
+}
+
+createAuthRefreshInterceptor(axiosInstance, refreshAuthLogic, {
+  statusCodes: [ 401 ],
+  retryInstance: axiosInstance
+});
+
+export default axiosInstance
