979c559e6998e4d77431ee98cdbbd9ffa630286e
Donghgo Gang <gongdongho12@gmail.com>
2020-07-16 13:51:38 +0900

    deploy terser webpack update & theme setting added & eslint added.

--- craco.config.js
+++ craco.config.js
@@ -4,11 +4,21 @@ const CracoLessPlugin = require('craco-less');
 const CracoAntDesignPlugin = require("craco-antd");
 const CracoAlias = require("craco-alias");
 const OpenBrowserPlugin = require("open-browser-webpack-plugin");
+const path = require("path");
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   reactScriptsVersion: "react-scripts",
   plugins: [
-    { plugin: CracoAntDesignPlugin },
+    {
+      plugin: CracoAntDesignPlugin,
+      options: {
+        customizeThemeLessPath: path.join(
+          __dirname,
+          "src/theme/customize.theme.less"
+        )
+      },
+    },
     { plugin: CracoLessPlugin },
     {
       plugin: CracoAlias,
@@ -21,6 +31,32 @@ module.exports = {
     },
     { plugin: new OpenBrowserPlugin({ url: 'http://localhost:3000' }) }
   ],
+  webpack: {
+    configure: webpackConfig => {
+      webpackConfig.optimization.minimizer = [
+        new TerserPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: true, // Must be set to true if using source-maps in production
+          terserOptions: {
+            compress: {
+              drop_console: true,
+            }
+          }
+        })
+      ]
+
+      return webpackConfig;
+    },
+    plugins: [
+        // new ConfigWebpackPlugin(),
+        // ...whenDev(() => [new CircularDependencyPlugin()], []),
+        // ...whenProd(() => [new WebpackClearConsole()], [])
+    ]
+  }
+  // eslint: {
+  //   mode: ESLINT_MODES.file
+  // },
   // style: {
   //     modules: {
   //         localIdentName: ""
