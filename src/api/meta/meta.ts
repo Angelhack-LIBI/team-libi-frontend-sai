74745b37b088493a489fbbe761b6f4afb9fccb1e
Dongho Gang <gongdongho12@gmail.com>
2020-07-19 17:03:07 +0900

    url fix

--- src/api/meta/meta.ts
+++ src/api/meta/meta.ts
@@ -1,6 +1,6 @@
 const config = {
   // baseURL: 'http://175.193.43.53:9999',
-  baseURL: 'https://libi-backend-2minchul.endpoint.ainize.ai/',
+  baseURL: 'https://libi-backend-2minchul.endpoint.ainize.ai',
   timeout: 2500,
   headers: {'Content-Type': 'application/json'}
 }
