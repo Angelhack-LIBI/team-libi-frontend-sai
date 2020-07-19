6ed342c94f279e187c97061d78ff5a5fdcb5e655
Dongho Gang <gongdongho12@gmail.com>
2020-07-19 21:54:26 +0900

    위도경도 Added.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent, useEffect } from "react";
 import "antd/dist/antd.less";
 import { IntlProvider } from "react-intl";
 import CommonRouter from "./CommonRouter";
+import queryString from 'query-string'
 import { BrowserRouter as Router } from "react-router-dom";
 
 import {
@@ -14,6 +15,12 @@ import axiosInstance from "api/AxiosInstance";
 import locationState from "state/location";
 import tokenState from "state/token";
 
+const locationOptions: any = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
 const App: FunctionComponent<any> = () => {
   const lang = useRecoilValue(langState);
   const token = useRecoilValue(tokenState);
@@ -21,27 +28,28 @@ const App: FunctionComponent<any> = () => {
   const [, setLocation] = useRecoilState<any>(locationState);
   
   useEffect(() => {
+    const success = (pos) => {
+      const { latitude: lat, longitude: long } = pos.coords;
+      axiosInstance.get(`/sharing/area/me?${queryString.stringify({ lat, long })}`)
+        .then(({ data }) => {
+          setLocation(data)
+        })
+    };
+    
+    const error = (err) => {
+      axiosInstance.get('/sharing/area/me')
+      .then(({ data }) => {
+        setLocation(data)
+      })
+    };
+
     console.log('token', token)
     if (token) {
-      axiosInstance.get('/sharing/area/me')
-        .then(({ data }) => {
-          setLocation(data)
-        })
-      navigator.geolocation.getCurrentPosition((position) => {
-      })
+      navigator.geolocation.getCurrentPosition(success, error, locationOptions);
     } else {
-      navigator.geolocation.getCurrentPosition((position) => {
-        axiosInstance.get('/sharing/area/me')
-        .then(({ data }) => {
-          setLocation(data)
-        })
-      });
+      navigator.geolocation.getCurrentPosition(success, () => {}, locationOptions);
     }
 
-    axiosInstance.get('/sharing/category')
-      .then(({ data }) => {
-        setCategory(data)
-      })
     return () => {
       setCategory([])
       setLocation({})
